4b1835b2aaa074b0225b73b727df5acb8f9f66ec
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-08-04 23:47:43 -0300

    Metadata

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,14 @@
 import SideNav from '@/app/ui/dashboard/sidenav'
+import { Metadata } from 'next'
 
 export const experimental_ppr = true
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Dashboard',
+  },
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
